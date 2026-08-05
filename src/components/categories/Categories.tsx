37fd76a5db8eb9cf26921af57f861c9e8036604f
Joe <muhammadjulian254@gmail.com>
2026-08-06 02:15:08 +0700

    adding more contents inside home page

--- src/components/categories/Categories.tsx
+++ src/components/categories/Categories.tsx
@@ -2,19 +2,19 @@ function Categories() {
   return (
     <div className="w-full h-9 bg-white ">
       <div className="h-full flex flex-row justify-center gap-20">
-        <div className="h-10 w-25 text-center border-b-4 border-white hover:border-blue-500 hover:text-blue-500 hover:cursor-pointer">
+        <div className="h-10 w-25 text-center border-b-4 border-white transition duration-300 hover:border-blue-500 hover:text-blue-500 hover:cursor-pointer">
           <p className="font-medium">Jerseys</p>
         </div>
-        <div className="h-10 w-30 text-center border-b-4 border-white hover:border-blue-500 hover:text-blue-500 hover:cursor-pointer">
+        <div className="h-10 w-30 text-center border-b-4 border-white transition duration-300 hover:border-blue-500 hover:text-blue-500 hover:cursor-pointer">
           <p className="font-medium">Popular</p>
         </div>
-        <div className="h-10 w-25 text-center border-b-4 border-white hover:border-blue-500 hover:text-blue-500 hover:cursor-pointer">
+        <div className="h-10 w-25 text-center border-b-4 border-white transition duration-300 hover:border-blue-500 hover:text-blue-500 hover:cursor-pointer">
           <p className="font-medium">Training</p>
         </div>
-        <div className="h-10 w-25 text-center border-b-4 border-white hover:border-blue-500 hover:text-blue-500 hover:cursor-pointer">
+        <div className="h-10 w-25 text-center border-b-4 border-white transition duration-300 hover:border-blue-500 hover:text-blue-500 hover:cursor-pointer">
           <p className="font-medium">Fashion</p>
         </div>
-        <div className="h-10 w-25 text-center border-b-4 border-white hover:border-blue-500 hover:text-blue-500 hover:cursor-pointer">
+        <div className="h-10 w-25 text-center border-b-4 border-white transition duration-300 hover:border-blue-500 hover:text-blue-500 hover:cursor-pointer">
           <p className="font-medium">Accessories</p>
         </div>
       </div>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,7 +2,12 @@ function Navbar() {
   return (
     <nav className="w-full h-30 bg-white">
       <div className="h-full grid grid-cols-3 items-center px-20">
-        <h1 className="text-4xl font-bold"><span className="text-blue-500">Ath</span>lora.</h1>
+        <div className="flex flex-col">
+          <h1 className="text-4xl font-bold">
+            <span className="text-blue-500">Ath</span>lora.
+          </h1>
+          <p className="text-xs">Athletic Lifestyle and Original Apparel</p>
+        </div>
         <form className="flex justify-center">
           <input
             type="text"
